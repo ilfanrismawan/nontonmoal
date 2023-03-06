729e004da98f4188bc87bc19251f8199fcc5a360
ilfanrismawan <irsthirteen@gmail.com>
2023-03-06 23:09:33 +0700

    navbar

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { BsChevronDown, BsSearch, BsBell } from "react-icons/bs";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { Menu } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 
 function NavigationBar() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -85,46 +85,85 @@ function NavigationBar() {
         <Menu>
           <Menu.Button>
             <img
-              // onClick={logout}
-
               src="https://rb.gy/g1pwyx"
               alt="Profile Picture"
               className="cursor-painter rounded"
             />
           </Menu.Button>
-          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="px-1 py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? "bg-violet-500 text-white" : "text-gray-900"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <div className="mr-2 h-5 w-5" aria-hidden="true"></div>
-                    ) : (
-                      <div className="mr-2 h-5 w-5" aria-hidden="true"></div>
-                    )}
-                    Edit
-                  </button>
-                )}
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-9 top-14 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-zinc-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="px-1 py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={() => router.push("/tv-shows")}
+                      className={`${
+                        active
+                          ? "bg-red-600 text-white"
+                          : "font-bold text-neutral-50"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      {active ? (
+                        <div className="mr-2 h-5 w-5" aria-hidden="true"></div>
+                      ) : (
+                        <div className="mr-2 h-5 w-5" aria-hidden="true"></div>
+                      )}
+                      Tv Shows
+                    </button>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={() => router.push("/movies")}
+                      className={`${
+                        active
+                          ? "bg-red-600 text-white"
+                          : "font-bold text-neutral-50"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      {active ? (
+                        <div className="mr-2 h-5 w-5" aria-hidden="true"></div>
+                      ) : (
+                        <div className="mr-2 h-5 w-5" aria-hidden="true"></div>
+                      )}
+                      Movies
+                    </button>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={() => router.push("/movies")}
+                      className={`${
+                        active
+                          ? "bg-red-600 text-white"
+                          : "font-bold text-neutral-50"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      {active ? (
+                        <div className="mr-2 h-5 w-5" aria-hidden="true"></div>
+                      ) : (
+                        <div className="mr-2 h-5 w-5" aria-hidden="true"></div>
+                      )}
+                      Popular
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+              <Menu.Item disabled>
+                <span className="opacity-75">Pengaturan</span>
               </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    className={`${active && "bg-blue-500"}`}
-                    href="/account-settings"
-                  >
-                    Documentation
-                  </a>
-                )}
-              </Menu.Item>
-            </div>
-            <Menu.Item disabled>
-              <span className="opacity-75">Invite a friend (coming soon!)</span>
-            </Menu.Item>
-          </Menu.Items>
+            </Menu.Items>
+          </Transition>
         </Menu>
       </div>
     </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import useMoviesList from "@/hooks/useMoviesList";
 import Billboard from "./components/Billboard";
+import Header from "./components/Header";
 import MovieList from "./components/MovieList";
 import NavigationBar from "./components/NavigationBar";
 
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <div className="relative h-screen lg:h-[140vh]">
       <NavigationBar />
+      {/* <Header /> */}
       <main className="relative pb-24 ">
         <Billboard />
 
